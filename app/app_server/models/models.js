e78ca9d7c8d68a3f48ecaac66bc4717c49122e76
claytonlocher <claytonlocher@gmail.com>
2016-02-26 18:46:37 -0600

    Define Mongoose schema

--- app/app_server/models/models.js
+++ app/app_server/models/models.js
@@ -1,20 +1,59 @@
-
 'use strict';
 
 const mongoose = require('mongoose');
 
+
+// Each location is "geometry" node of a GeoJSON object
 var locationSchema = new mongoose.Schema({
-  // Define schema for locations here
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [Number]
+  }
+});
+
+var projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String
+  },
+  projectLink: {
+    type: String
+  },
+  fullText: {
+    type: String
+  },
+  tags: {
+    type: Array
+  },
+  partners: [researcherSchema]
+  locations: [locationSchema]
+});
+
+var researcherSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   email: {
     type: String,
-    required: true
+  },
+  photoLink: {
+    type: String
   }
-
-
+  profileLink: {
+    type: String,
+    default: 
+  },
+  researchAreas: {
+    type: Array
+  },
+  projects: [projectSchema]
 });
 
-mongoose.model(locationSchema);
+mongoose.model(researcherSchema);
