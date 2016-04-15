9a7dcf20b2caaa4e5e45346846b62d2ea400db5c
Edison Orellana <annuit02@gmail.com>
2016-04-15 18:09:25 -0500

    added gdal dependency for use with other function

---
+++ app/scripts/convertToGeoJSON.js
@@ -0,0 +1,9 @@
+// Convert multiple spatial data formats to GeoJSON
+
+// ogr2ogr -f GeoJSON -t_srs crs:84 [name].geojson [name].shp
+
+function convert(parameter3) {
+    // use ogr2ogr to convert to GeoJSON
+     
+}
+
