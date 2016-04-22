081a9c8971f2dc73e5787295f8cfc3d5d994ecfe
Edison Orellana <annuit02@gmail.com>
2016-04-21 19:48:45 -0500

    added conversion functionality. Need to fix geojson specification though

--- app/scripts/convertToGeoJSON.js
+++ app/scripts/convertToGeoJSON.js
@@ -2,8 +2,22 @@
 
 // ogr2ogr -f GeoJSON -t_srs crs:84 [name].geojson [name].shp
 
-function convert(parameter3) {
-    // use ogr2ogr to convert to GeoJSON
-     
+var gdal = require("gdal");
+var fs = require("fs");
+
+
+var shp = gdal.open("./champaign/champaign.shp")
+
+function convert() {
+        shp.layers.get(0).features.forEach(function(feature) {
+        //console.log(feature.getGeometry().toJSON());
+        var outFeature = feature.getGeometry().toJSON();
+        fs.appendFile('./area.JSON', outFeature, function(err) {
+            if(err){
+                console.log(err);
+            }
+        })
+    });
 }
 
+convert();
